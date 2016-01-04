57ad4875562674180f2f6f794df4025ab965d5d5
cj <christopherwjohnson@gmail.com>
2016-01-04 13:04:20 -0700

    Accidentally added automation with a .git folder so it was not fully added.

--- cmake-tools.cmake
+++ cmake-tools.cmake
@@ -1,1 +1,9 @@
-include(scripts/project.cmake)
+set(CMAKE_UTILITY ${CMAKE_CURRENT_LIST_DIR})
+include(${CMAKE_UTILITY}/scripts/print-info.cmake)
+
+if (ANDROID)
+    include(${CMAKE_UTILITY}/scripts/android.cmake)
+else ()
+  include(${CMAKE_UTILITY}/scripts/project.cmake)
+endif ()
+
--- scripts/project.cmake
+++ scripts/project.cmake
@@ -1,5 +1,3 @@
-set(CMAKE_UTILITY ..)
-include(print-info.cmake)
 
 macro(create_library target)
   set(CURRENT_TARGET ${target})
@@ -11,18 +9,27 @@ macro(create_library target)
 
   if ("${ARGN}" STREQUAL "")
     #    message("No sources for ${target}")
-    file(GLOB_RECURSE SOURCES source/*.cpp source/*.c)
+    file(GLOB_RECURSE SOURCES source/*.cpp source/*.mm source/*.m source/*.c)
+
     file(GLOB_RECURSE HEADERS source/*.h)
     add_library(${target} ${SOURCES} ${HEADERS})
   else ()
     add_library(${target} ${ARGN})
   endif ()
 
+  if (NOT ANDROID)
+    set("TARGET_FILE_DIR_${target}" $<TARGET_FILE_DIR:${target}>)
+    set("TARGET_LINKER_FILE_${target}" $<TARGET_LINKER_FILE:${target}>)
+    set("TARGET_FILE_${target}" $<TARGET_FILE:${target}>)
+  endif ()
+
   string(LENGTH "${CMAKE_SOURCE_DIR}" string_length)
   math(EXPR string_length "${string_length} + 1")
-  string(SUBSTRING .. ${string_length} -1 current_path)
+  string(SUBSTRING ${CMAKE_CURRENT_SOURCE_DIR} ${string_length} -1 current_path)
   get_filename_component(current_path ${current_path} DIRECTORY)
-  set_target_properties(${target} PROPERTIES FOLDER ${current_path})
+  if (NOT ANDROID)
+    set_target_properties(${target} PROPERTIES FOLDER ${current_path})
+  endif ()
   #      message( "${current_path} ${temp}")
 
   include_directories(${CMAKE_UTILITY}/include) # for dllexport
@@ -34,6 +41,12 @@ macro(create_library target)
   endif (IOS)
 
 endmacro(create_library)
+
+macro(get_relative_path result root_path path)
+  string(LENGTH "${root_path}" string_length)
+  math(EXPR string_length "${string_length} + 1")
+  string(SUBSTRING ${path} ${string_length} -1 ${result})
+endmacro(get_relative_path)
 
 macro(create_test target)
   set(LAST_TARGET ${CURRENT_TARGET})
@@ -48,7 +61,7 @@ macro(create_test target)
 
   string(LENGTH "${CMAKE_SOURCE_DIR}" string_length)
   math(EXPR string_length "${string_length} + 1")
-  string(SUBSTRING .. ${string_length} -1 current_path)
+  string(SUBSTRING ${CMAKE_CURRENT_SOURCE_DIR} ${string_length} -1 current_path)
   get_filename_component(current_path ${current_path} DIRECTORY)
   set_target_properties(${target} PROPERTIES FOLDER ${current_path})
 
@@ -58,16 +71,16 @@ endmacro(create_test)
 
 macro(require)
   foreach (library_name ${ARGN})
-#    message("${PROJECT_NAME} require ${library_name}")
+    #    message("${PROJECT_NAME} require ${library_name}")
     find_package(${library_name} REQUIRED)
 
     if (IOS)
       target_link_libraries(${CURRENT_TARGET}
         $<TARGET_FILE:${library_name}>
         )
-    else ()
+    elseif (NOT ANDROID)
       target_link_libraries(${CURRENT_TARGET}
-        $<TARGET_LINKER_FILE:${library_name}>
+        $TARGET_LINKER_FILE_${library_name}
         )
     endif ()
 
@@ -87,7 +100,7 @@ if (IOS)
 
     project(${project_name})
 
-    set(${project_name}_DIR .. PARENT_SCOPE)
+    set(${project_name}_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
 
     include(${project_name}-config.cmake)
   endmacro(add_project)
@@ -119,7 +132,7 @@ else ()
 
     project(${project_name})
 
-    set(${project_name}_DIR .. PARENT_SCOPE)
+    set(${project_name}_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
 
     include(${project_name}-config.cmake)
 
@@ -128,13 +141,15 @@ else ()
   macro(require_package project_name library_name)
     find_package(${library_name} REQUIRED)
 
-    target_link_libraries(${project_name}
-      $<TARGET_LINKER_FILE:${library_name}>
-      )
+    if (NOT ANDROID)
+      target_link_libraries(${project_name}
+        $TARGET_LINKER_FILE_${library_name}
+        )
 
-    add_dependencies(${project_name}
-      ${library_name}
-      )
+      add_dependencies(${project_name}
+        ${library_name}
+        )
+    endif ()
 
   endmacro(require_package)
 
@@ -144,3 +159,35 @@ endif ()
 macro(add name)
   add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/${name})
 endmacro(add)
+
+macro(add_resources resources_dir)
+
+  if (IOS)
+    file(GLOB_RECURSE BUNDLE_RESOURCES ${resources_dir}/*)
+    add_executable("${CURRENT_TARGET}_resources" MACOSX_BUNDLE ${BUNDLE_RESOURCES})
+    get_filename_component(base_path ${resources_dir} ABSOLUTE)
+
+    foreach (resource_path ${BUNDLE_RESOURCES})
+      #message("hello ${base_path}, ${resource_path}")
+      get_filename_component(resource_dir ${resource_path} DIRECTORY)
+      get_relative_path(relative_dir ${base_path} ${resource_dir})
+      message("resource ${resource_path}")
+      message("resource ${relative_dir}")
+      #set_source_files_properties(${BUNDLE_RESOURCES} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
+
+    endforeach ()
+    #set_source_files_properties(${BUNDLE_RESOURCES} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
+    #SET_TARGET_PROPERTIES("${CURRENT_TARGET}_resources" PROPERTIES LINKER_LANGUAGE C)
+    set(ALL_RESOURCES "${ALL_RESOURCES} BUNDLE_RESOURCES" PARENT_SCOPE)
+  else ()
+
+    add_custom_command(TARGET ${CURRENT_TARGET} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+      ${CMAKE_CURRENT_LIST_DIR}/${resource_dir} $TARGET_FILE_DIR_${CURRENT_TARGET}/${resource_dir})
+  endif ()
+
+endmacro(add_resources)
+
+macro(finish_mythic)
+
+endmacro(finish_mythic)
