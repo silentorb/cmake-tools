e1b3d1b59da932048058cbc17b8ad5307ea4a254
cj <cj@silentorb.com>
2016-03-29 20:34:50 -0600

    Made setting compiler options across projects possible.

--- scripts/project.cmake
+++ scripts/project.cmake
@@ -9,7 +9,13 @@ macro(create_library target)
   set (CMAKE_OSX_ARCHITECTURES "armv7 arm64")
 
   # Optimize debug build to help diagnose release build crashes.
-  #set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Os")
+    if (GCC_DEBUG_OPTIMIZATION_LEVEL)
+      set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -${GCC_DEBUG_OPTIMIZATION_LEVEL}")
+    endif()
+
+  if (MSVC_DEBUG_FLAGS)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${MSVC_DEBUG_FLAGS}")
+  endif()
 
   set(CURRENT_TARGET ${target})
   #message(WARNING "*${PROJECT_NAME} STREQUAL ${target}*")
