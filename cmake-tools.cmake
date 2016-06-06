a8926184e2f4ce563cd0461b7dadf508c3562f7a
cj <cj@silentorb.com>
2016-06-06 10:41:17 -0600

    Running into too many undocumented chasms to cross using the ndk cmake tools.  Going back to generating ndk-build make files, which worked.

--- cmake-tools.cmake
+++ cmake-tools.cmake
@@ -1,9 +1,13 @@
 set(CMAKE_TOOLS ${CMAKE_CURRENT_LIST_DIR})
 include(${CMAKE_TOOLS}/scripts/print-info.cmake)
 
-#if (ANDROID)
-#  include(${CMAKE_TOOLS}/scripts/android.cmake)
-#else ()
+if (ANDROID_NDK)
+  set(ANDROID 1)
+endif ()
+
+if (ANDROID)
+  include(${CMAKE_TOOLS}/scripts/android.cmake)
+else ()
   include(${CMAKE_TOOLS}/scripts/project.cmake)
-#endif ()
+endif ()
 
