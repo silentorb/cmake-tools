4c52dc3daffe933bfa6bf35f6586bf3a5f159f1c
cj <cj@silentorb.com>
2017-06-26 00:14:15 -0600

    Building external packages for windows is working.  It will build both mingw and msvc bin/headers in one call.

--- scripts/external-package.cmake
+++ scripts/external-package.cmake
@@ -1,38 +1,37 @@
 
 function(platform_build_external key generator)
-  file(MAKE_DIRECTORY build)
   set(build_path "build/${key}")
+  set(bin_path "bin/${key}")
   file(MAKE_DIRECTORY ${build_path})
+  file(MAKE_DIRECTORY ${bin_path})
+
   if (generator)
-    execute_process(COMMAND cmake -G ${generator} ../../cmake
-      WORKING_DIRECTORY ${build_path}
-      RESULT_VARIABLE output
-      )
-  else ()
-    execute_process(COMMAND cmake -DCMAKE_INSTALL_PREFIX=${parent_dir} ../../cmake
-      WORKING_DIRECTORY ${build_path}
-      RESULT_VARIABLE output
-      )
+    set(generator_flag "-G")
   endif ()
+
+  execute_process(COMMAND cmake ${generator_flag} ${generator} -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/bin/${key} ../../cmake
+    WORKING_DIRECTORY ${build_path}
+    RESULT_VARIABLE output
+    )
   message(${output})
 
-#  execute_process(COMMAND cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/bin/msvc --build .
-#    WORKING_DIRECTORY ${build_path}
-#    RESULT_VARIABLE output
-#    )
-#  message(${output})
-#
-#    execute_process(COMMAND cmake -P cmake_install.cmake
-#      WORKING_DIRECTORY ${build_path}
-#      RESULT_VARIABLE output
-#      )
-#    message(${output})
+  if (MSVC)
+    set(config_flag "--config")
+    set(config_value RELWITHDEBINFO)
+  endif ()
+
+  execute_process(COMMAND cmake --build . ${config_flag} ${config_value}
+    WORKING_DIRECTORY ${build_path}
+    RESULT_VARIABLE output
+    )
+  message(${output})
+
 endfunction()
 
 function(build_external)
   if (WIN32)
     platform_build_external(mingw "MinGW Makefiles")
-    #    platform_build_external(msvc)
+    platform_build_external(msvc "")
   endif ()
 
 endfunction()
